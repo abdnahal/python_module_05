c65730601250c228a255b1859fac771a52b46e38
Abdellatif Nahal <abdnahal@c3r8p3.1337.ma>
2026-02-28 02:21:53 +0000

    ex1

--- ex1/data_stream.py
+++ ex1/data_stream.py
@@ -1,21 +1,58 @@
 from abc import ABC, abstractmethod
+from typing import Any, List, Optional, Dict, Union
 
 
 class DataStream(ABC):
     def __init__(self, stream_id: str, stream_type: str):
         self.stream_id = stream_id
         self.stream_type = stream_type
-        
 
+    @abstractmethod
+    def process_batch(self, data_batch: List[Any]) -> str:
+        pass
+
+    @abstractmethod
+    def filter_data(self, data_batch: List[Any],
+                    criteria: Optional[str] = None) -> List[Any]:
+        pass
+
+    @abstractmethod
+    def get_stats(self) -> Dict[str, Union[str, int, float]]:
+        pass
+     
 
 class SensorStream(DataStream):
-    pass
+    def process_batch(self, data_batch: List[Any]) -> str:
+        pass
+
+    def filter_data(self, data_batch: List[Any],
+                    criteria: Optional[str] = None) -> List[Any]:
+        pass
+
+    def get_stats(self) -> Dict[str, Union[str, int, float]]:
+        pass
 
 
 class TransactionStream(DataStream):
-    pass
+    def process_batch(self, data_batch: List[Any]) -> str:
+        pass
+
+    def filter_data(self, data_batch: List[Any],
+                    criteria: Optional[str] = None) -> List[Any]:
+        pass
+
+    def get_stats(self) -> Dict[str, Union[str, int, float]]:
+        pass
 
 
 class EventStream(DataStream):
-    pass
+    def process_batch(self, data_batch: List[Any]) -> str:
+        pass
+
+    def filter_data(self, data_batch: List[Any],
+                    criteria: Optional[str] = None) -> List[Any]:
+        pass
+
+    def get_stats(self) -> Dict[str, Union[str, int, float]]:
+        pass
 
