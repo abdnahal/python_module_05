4db94e0ed24eedd24a89162babf619667c285513
Abdellatif Nahal <abdnahal@e1r12p11.1337.ma>
2026-03-01 02:56:50 +0000

    ex1

--- ex1/data_stream.py
+++ ex1/data_stream.py
@@ -6,6 +6,7 @@ class DataStream(ABC):
     def __init__(self, stream_id: str, stream_type: str):
         self.stream_id = stream_id
         self.stream_type = stream_type
+        self.process_count = 0
 
     @abstractmethod
     def process_batch(self, data_batch: List[Any]) -> str:
@@ -14,19 +15,43 @@ class DataStream(ABC):
     @abstractmethod
     def filter_data(self, data_batch: List[Any],
                     criteria: Optional[str] = None) -> List[Any]:
-        pass
+        if criteria:
+            filtered = [item for item in data_batch if criteria in item]
+            return filtered
+        else:
+            return data_batch
 
     @abstractmethod
     def get_stats(self) -> Dict[str, Union[str, int, float]]:
-        pass
-     
+        stats = dict()
+        stats["stream_id"] = self.stream_id
+        stats["stream_type"] = self.stream_type
+        stats["processed"] = self.process_count
+        return stats
+
 
 class SensorStream(DataStream):
     def __init__(self, stream_id, stream_type):
         super().__init__(stream_id, stream_type)
 
     def process_batch(self, data_batch: List[Any]) -> str:
-        pass
+        data = dict()
+        try:
+            if not isinstance(data_batch, list):
+                raise TypeError("Invalid data_batch type")
+            for item in data_batch:
+                if not isinstance(item, str):
+                    raise TypeError("Invalid item type")
+                else:
+                    parts = item.split(":")
+                    data[parts[0]] += round(float(parts[1]), 1)
+
+        except TypeError as e:
+            return e
+        read = len(data.keys())
+        avg = data["temp"]
+        self.process_count += len(data_batch)
+        return f"Sensor analysis: {read} readings processed, avg temp: {avg}°C"
 
     def filter_data(self, data_batch: List[Any],
                     criteria: Optional[str] = None) -> List[Any]:
