3a8b9a4cffaf7d39c83682a4f803e597f3285b15
Abdellatif Nahal <abdnahal@e1r12p11.1337.ma>
2026-03-02 21:31:47 +0000

    ex2

--- ex1/data_stream.py
+++ ex1/data_stream.py
@@ -46,8 +46,8 @@ class SensorStream(DataStream):
                     parts = item.split(":")
                     data[parts[0]] += round(float(parts[1]), 1)
 
-        except TypeError as e:
-            return e
+        except (TypeError, ValueError) as e:
+            return f"Error processing Sensor data: {e}"
         read = len(data.keys())
         avg = data["temp"]
         self.process_count += len(data_batch)
@@ -66,7 +66,29 @@ class TransactionStream(DataStream):
         super().__init__(stream_id, stream_type)
 
     def process_batch(self, data_batch: List[Any]) -> str:
-        pass
+        net = 0
+        try:
+            if not isinstance(data_batch, list):
+                raise TypeError("Invalid data_batch type")
+            for item in data_batch:
+                if not isinstance(item, str):
+                    raise TypeError("Invalid item type")
+                else:
+                    parts = item.split(":")
+                    if "buy" != parts[0]:
+                        if "sell" != parts[0]:
+                            e = "Invalid item, expected buy or sell"
+                            raise ValueError(e)
+                        else:
+                            net -= int(parts[1])
+                    else:
+                        net += int(parts[1])
+        except (TypeError, ValueError) as e:
+            return f"Error processing Transaction data: {e}"
+        self.process_count += len(data_batch)
+        return f"Transaction analysis: {len(data_batch)} operations,\
+net flow: {net} units"
+
 
     def filter_data(self, data_batch: List[Any],
                     criteria: Optional[str] = None) -> List[Any]:
@@ -81,7 +103,19 @@ class EventStream(DataStream):
         super().__init__(stream_id, stream_type)
 
     def process_batch(self, data_batch: List[Any]) -> str:
-        pass
+        try:
+            if not isinstance(data_batch, list):
+                raise TypeError("Invalid data_batch type")
+            for item in data_batch:
+                if not isinstance(item, str):
+                    raise TypeError("Invalid item type")
+        except TypeError as e:
+            return f"Error processing event data: {e}"
+        total = len(data_batch)
+        err_count = sum(1 for item in data_batch if item == "error")
+        self.process_count += len(data_batch)
+        return f"{total} events, {err_count} error detected"
+            
 
     def filter_data(self, data_batch: List[Any],
                     criteria: Optional[str] = None) -> List[Any]:
